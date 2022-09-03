4236739401f4ae9981ed23566a5cc44063147921
Emre Toraman <emretoraman@gmail.com>
2022-09-03 19:50:36 +0300

    Add PutRental

--- VacationRental.Api/Controllers/RentalsController.cs
+++ VacationRental.Api/Controllers/RentalsController.cs
@@ -8,10 +8,12 @@ namespace VacationRental.Api.Controllers
     public class RentalsController : ControllerBase
     {
         private readonly IDictionary<int, RentalViewModel> _rentals;
+        private readonly IDictionary<int, BookingViewModel> _bookings;
 
-        public RentalsController(IDictionary<int, RentalViewModel> rentals)
+        public RentalsController(IDictionary<int, RentalViewModel> rentals, IDictionary<int, BookingViewModel> bookings)
         {
             _rentals = rentals;
+            _bookings = bookings;
         }
 
         [HttpGet]
@@ -42,6 +44,56 @@ namespace VacationRental.Api.Controllers
             });
 
             return key;
+        }
+
+        [HttpPut]
+        [Route("{rentalId:int}")]
+        public ResourceIdViewModel Put(int rentalId, RentalBindingModel model)
+        {
+            if (model.Units < 0)
+                throw new ApplicationException("Units must be positive");
+            if (model.PreparationTimeInDays < 0)
+                throw new ApplicationException("PreparationTimeInDays must be positive");
+            if (!_rentals.ContainsKey(rentalId))
+                throw new ApplicationException("Rental not found");
+
+            var rental = _rentals[rentalId];
+
+            if (model.Units < rental.Units || model.PreparationTimeInDays > rental.PreparationTimeInDays)
+            {
+                //Get only related bookings
+                var bookings = _bookings.Values
+                    .Where(b => b.RentalId == rentalId)
+                    .ToList();
+
+                var blockedUnitCounts = new Dictionary<DateTime, int>();
+                foreach (var booking in bookings)
+                {
+                    var totalDays = booking.Nights + model.PreparationTimeInDays;
+                    for (var i = 0; i < totalDays; i++)
+                    {
+                        var date = booking.Start.Date.AddDays(i);
+                        if (!blockedUnitCounts.ContainsKey(date))
+                        {
+                            blockedUnitCounts[date] = 1;
+                        }
+                        else
+                        {
+                            blockedUnitCounts[date]++;
+                        }
+
+                        if (blockedUnitCounts[date] > model.Units)
+                        {
+                            throw new ApplicationException("Failed due to overlappings");
+                        }
+                    }
+                }
+            }
+
+            rental.Units = model.Units;
+            rental.PreparationTimeInDays = model.PreparationTimeInDays;
+
+            return new ResourceIdViewModel { Id = rentalId };
         }
     }
 }
